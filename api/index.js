9980623a9b402982f045746a9484cad62c76c9b8
SaqlainZaidi1109 <saqlainzaidi1109@gmail.com>
2024-04-12 04:11:07 +0500

    Connect to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config(); //to connect .env file to database, learn more in .env file
+
+mongoose.connect(process.env.MONGOCSTRING) //since we dont have connection string here, people dont get to see it on github and anywhere else etc. COnnection string is in env
+.then(()=>{
+    console.log("MongoDB is connected")
+})
+.catch((err)=>{console.log(err)})
 
 const app = express();
 
